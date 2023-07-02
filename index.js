c9678d5b95e019cd77bdcb711d4639a23ce061d2
Edward <ed.f.jones@gmail.com>
2023-07-02 16:41:26 -0400

    setup additonalcomponents for local storage but need to find the bug that shows the array

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import './src/cart/toggleCart.js';
 import './src/cart/setupCart.js';
 // specific imports
 import fetchProducts from './src/fetchProducts.js';
-import  {setupStore, store} from './src/store.js';
+import  {findProduct, setupStore, store} from './src/store.js';
 //import display from './src/displayProducts.js';
 //import { getElement } from './src/utils.js';
 
@@ -12,7 +12,8 @@ const init = async () => {
     const products = await fetchProducts();
     if(products) {
         setupStore(products);
-        console.log(store);
+        const featured = store.filter((product) => findProduct.featured === true);
+        console.log(featured);
     }
 }
 
